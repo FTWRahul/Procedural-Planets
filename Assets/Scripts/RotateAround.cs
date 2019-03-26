f1f3bfb64e1c632b01a17766aef95fdc20429694
FTWRahul <r.poundarik@gmail.com>
2019-03-26 13:15:52 -0400

    New planet made

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = Vector3.Slerp(Vector3.up, Vector3.down, rotationSpeed);
+        transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
